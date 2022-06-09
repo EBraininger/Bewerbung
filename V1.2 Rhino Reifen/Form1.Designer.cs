5f1c95a3dcf06cad5640ef89636cf965a4872158
Erik Braininger <70947754+EBraininger@users.noreply.github.com>
2022-06-09 15:08:54 +0200

    Eine Object detection

---
+++ V1.2 Rhino Reifen/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace V1._2_Rhino_Reifen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbGreen = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbBlue = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sbGreenColor = new System.Windows.Forms.HScrollBar();
+            this.rbRed = new System.Windows.Forms.RadioButton();
+            this.sbRedColor = new System.Windows.Forms.HScrollBar();
+            this.sbBlueColor = new System.Windows.Forms.HScrollBar();
+            this.sbRadius = new System.Windows.Forms.HScrollBar();
+            this.sbThreshold = new System.Windows.Forms.HScrollBar();
+            this.cbBlur = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(406, 115);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(418, 408);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(263, 115);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(116, 115);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(263, 250);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(116, 115);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(263, 408);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(116, 115);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(189, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(189, 212);
+            this.listBox1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbGreen);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.rbBlue);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.sbGreenColor);
+            this.groupBox1.Controls.Add(this.rbRed);
+            this.groupBox1.Controls.Add(this.sbRedColor);
+            this.groupBox1.Controls.Add(this.sbBlueColor);
+            this.groupBox1.Controls.Add(this.sbRadius);
+            this.groupBox1.Controls.Add(this.sbThreshold);
+            this.groupBox1.Location = new System.Drawing.Point(12, 369);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(189, 191);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Option";
+            // 
+            // rbGreen
+            // 
+            this.rbGreen.AutoSize = true;
+            this.rbGreen.Location = new System.Drawing.Point(5, 164);
+            this.rbGreen.Name = "rbGreen";
+            this.rbGreen.Size = new System.Drawing.Size(48, 17);
+            this.rbGreen.TabIndex = 20;
+            this.rbGreen.TabStop = true;
+            this.rbGreen.Text = "Grün";
+            this.rbGreen.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Raudis";
+            // 
+            // rbBlue
+            // 
+            this.rbBlue.AutoSize = true;
+            this.rbBlue.Location = new System.Drawing.Point(5, 125);
+            this.rbBlue.Name = "rbBlue";
+            this.rbBlue.Size = new System.Drawing.Size(46, 17);
+            this.rbBlue.TabIndex = 20;
+            this.rbBlue.TabStop = true;
+            this.rbBlue.Text = "Blau";
+            this.rbBlue.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Threshold";
+            // 
+            // sbGreenColor
+            // 
+            this.sbGreenColor.Location = new System.Drawing.Point(61, 157);
+            this.sbGreenColor.Maximum = 255;
+            this.sbGreenColor.Name = "sbGreenColor";
+            this.sbGreenColor.Size = new System.Drawing.Size(125, 22);
+            this.sbGreenColor.TabIndex = 17;
+            this.sbGreenColor.Value = 30;
+            // 
+            // rbRed
+            // 
+            this.rbRed.AutoSize = true;
+            this.rbRed.Checked = true;
+            this.rbRed.Location = new System.Drawing.Point(5, 88);
+            this.rbRed.Name = "rbRed";
+            this.rbRed.Size = new System.Drawing.Size(42, 17);
+            this.rbRed.TabIndex = 19;
+            this.rbRed.TabStop = true;
+            this.rbRed.Text = "Rot";
+            this.rbRed.UseVisualStyleBackColor = true;
+            // 
+            // sbRedColor
+            // 
+            this.sbRedColor.Location = new System.Drawing.Point(61, 88);
+            this.sbRedColor.Maximum = 255;
+            this.sbRedColor.Name = "sbRedColor";
+            this.sbRedColor.Size = new System.Drawing.Size(125, 22);
+            this.sbRedColor.TabIndex = 17;
+            this.sbRedColor.Value = 220;
+            // 
+            // sbBlueColor
+            // 
+            this.sbBlueColor.Location = new System.Drawing.Point(61, 125);
+            this.sbBlueColor.Maximum = 255;
+            this.sbBlueColor.Name = "sbBlueColor";
+            this.sbBlueColor.Size = new System.Drawing.Size(125, 22);
+            this.sbBlueColor.TabIndex = 17;
+            this.sbBlueColor.Value = 30;
+            // 
+            // sbRadius
+            // 
+            this.sbRadius.Location = new System.Drawing.Point(61, 52);
+            this.sbRadius.Maximum = 450;
+            this.sbRadius.Name = "sbRadius";
+            this.sbRadius.Size = new System.Drawing.Size(125, 22);
+            this.sbRadius.TabIndex = 17;
+            this.sbRadius.Value = 100;
+            // 
+            // sbThreshold
+            // 
+            this.sbThreshold.Location = new System.Drawing.Point(61, 18);
+            this.sbThreshold.Maximum = 255;
+            this.sbThreshold.Name = "sbThreshold";
+            this.sbThreshold.Size = new System.Drawing.Size(125, 22);
+            this.sbThreshold.TabIndex = 17;
+            this.sbThreshold.Value = 40;
+            // 
+            // cbBlur
+            // 
+            this.cbBlur.AutoSize = true;
+            this.cbBlur.Location = new System.Drawing.Point(18, 333);
+            this.cbBlur.Name = "cbBlur";
+            this.cbBlur.Size = new System.Drawing.Size(99, 17);
+            this.cbBlur.TabIndex = 21;
+            this.cbBlur.Text = "Blur hinzufügen";
+            this.cbBlur.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(545, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 26);
+            this.textBox1.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 589);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cbBlur);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbGreen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbBlue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.HScrollBar sbGreenColor;
+        private System.Windows.Forms.RadioButton rbRed;
+        private System.Windows.Forms.HScrollBar sbRedColor;
+        private System.Windows.Forms.HScrollBar sbBlueColor;
+        private System.Windows.Forms.HScrollBar sbRadius;
+        private System.Windows.Forms.HScrollBar sbThreshold;
+        private System.Windows.Forms.CheckBox cbBlur;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ V1.2 Rhino Reifen/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace V1._2_Rhino_Reifen
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection _device;
+        VideoCaptureDevice _CaptureDevice;
+        Bitmap _bitmapEdgeImage, _bitmapBinaryImage, _bitmapGreyImage, _bitmapBlurImage, _colorFilterImage;
+        //ColorFiltering _colorFilter = new ColorFiltering();
+        EuclideanColorFiltering _colorFilter = new EuclideanColorFiltering();
+        System.Drawing.Font _font = new System.Drawing.Font("Times New Roman", 48, FontStyle.Bold);
+        System.Drawing.SolidBrush _brush = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
+        SobelEdgeDetector _edgeFilter = new SobelEdgeDetector();
+        Pen _PictureboxPen = new Pen(Color.Black, 5);
+
+        bool _blurFlag = false;
+        int ipenWidth = 5, iFeatureWidth;
+        int iThreshold = 40, iRadius = 40;
+        int iColorMode = 1, iRedValue = 220, iGreenValue = 30, iBlueValue = 30;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                _device = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                for (var i = 0; i < _device.Count; i++)
+                    comboBox1.Items.Add(_device[i].Name);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
